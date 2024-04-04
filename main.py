5426b940cf18142d6c4052895138e7987f4315b2
Himani170 <145888193+Himani170@users.noreply.github.com>
2024-04-04 20:46:44 +0530

    make changes
    attached the assests folder and the main menu still play cant run the actual game. need some little work over there

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ import sys
 from modules.player import Player
 from modules.world import Obstacle
 from modules.events import *
+from button import Button 
+
+pygame.init()
+
+SCREEN = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Menu")
+
+BG = pygame.image.load("C:/Users/Dell/Downloads/Menu-System-PyGame-main/Menu-System-PyGame-main/assets/Background.png")
+
+def get_font(size): # Returns Press-Start-2P in the desired size
+    return pygame.font.Font("C:/Users/Dell/Downloads/Menu-System-PyGame-main/Menu-System-PyGame-main/assets/font.ttf", size)
+
 
 SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 600
@@ -110,8 +122,41 @@ def play():
     pygame.quit()
     sys.exit()
 
-def main():
-    play()
+def main_menu():
+    while True:
+        SCREEN.blit(BG, (0, 0))
 
-if __name__ == "__main__":
-    main()
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+        PLAY_BUTTON = Button(image=pygame.image.load("C:/Users/Dell/Downloads/Menu-System-PyGame-main/Menu-System-PyGame-main/assets/Play Rect.png"), pos=(640, 250), 
+                            text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BUTTON = Button(image=pygame.image.load("C:/Users/Dell/Downloads/Menu-System-PyGame-main/Menu-System-PyGame-main/assets/Options Rect.png"), pos=(640, 400), 
+                            text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("C:/Users/Dell/Downloads/Menu-System-PyGame-main/Menu-System-PyGame-main/assets/Quit Rect.png"), pos=(640, 550), 
+                            text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+
+        SCREEN.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(SCREEN)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    play()
+                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    options()
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+
+main_menu()
